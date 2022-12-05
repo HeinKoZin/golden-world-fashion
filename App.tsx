14c67f67daa0775a0b29d4aa2bc42cd166d607ee
Hein Ko Zin <heinkozin4@gmail.com>
2022-12-05 08:53:58 +0630

    ✨feature: app name change

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import WebView from "react-native-webview";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { Component } from "react";
+import { BackHandler } from "react-native";
+
+export default class App extends Component {
+	WEBVIEW_REF: React.RefObject<any>;
+	constructor(props: {} | Readonly<{}>) {
+		super(props);
+		this.WEBVIEW_REF = React.createRef();
+	}
+
+	componentDidMount() {
+		BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
+	}
+
+	componentWillUnmount() {
+		BackHandler.removeEventListener("hardwareBackPress", this.handleBackButton);
+	}
+
+	handleBackButton = () => {
+		this.WEBVIEW_REF.current.goBack();
+		return true;
+	};
+
+	onNavigationStateChange(navState: { canGoBack: any }) {
+		this.setState({
+			canGoBack: navState.canGoBack,
+		});
+	}
+
+	render(): React.ReactNode {
+		return (
+			<SafeAreaView>
+				<View style={styles.container}>
+					<StatusBar style="auto" />
+					<WebView
+						source={{ uri: "https://www.goldenworldfashion.com/" }}
+						ref={this.WEBVIEW_REF}
+						onNavigationStateChange={this.onNavigationStateChange.bind(this)}
+					/>
+				</View>
+			</SafeAreaView>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		width: "100%",
+		height: "100%",
+	},
 });
